6f1084fc3f227aa142ee0195a8e4f007e32f875b
sergeytsarev90 <44232706+sergeytsarev90@users.noreply.github.com>
2019-02-17 20:45:00 +0300

    Урок 1 задание 1 выполнено

---
+++ lesson_1_1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import unittest
+
+
+class AppDynamicsJob(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+
+
+    def test_app_dynamics_job(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/group.php")
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").send_keys("qwe")
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Groups'])[1]/following::form[1]").click()
+        driver.find_element_by_link_text("Logout").click()
+        pass
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+if __name__ == "__main__":
+    unittest.main()
