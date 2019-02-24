2c2726a834ea72a71b6f7d3b5c52189cec904bca
sergeytsarev90 <44232706+sergeytsarev90@users.noreply.github.com>
2019-02-24 20:07:06 +0300

    Урок 1 задание 3 выполнено

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+
+    def __init__(self,firstname,middlename,lastname,nickname,mobile,email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.mobile = mobile
+        self.email = email
--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import unittest
 from group import Group
+from contact import Contact
 
 
 class AppDynamicsJob(unittest.TestCase):
@@ -11,13 +12,21 @@ class AppDynamicsJob(unittest.TestCase):
         self.driver.implicitly_wait(30)
 
 
-    def test_app_dynamics_job(self):
+    def test_add_group(self):
         driver = self.driver
         self.open_home_page(driver)
         self.login(driver)
         self.open_group_page(driver)
         self.create_group(driver, Group(name="new_group",header="new_header",footer="new_footer"))
         self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_contact(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver)
+        self.open_add_contact_page(driver)
+        self.create_contact(driver, Contact(firstname="Ivan",middlename="Ivanovich",lastname="Ivanov",nickname="Ivan",mobile="8915439323",email="ivanov@ya.ru"))
         self.logout(driver)
 
     def return_to_groups_page(self, driver):
@@ -46,12 +55,20 @@ class AppDynamicsJob(unittest.TestCase):
     def open_home_page(self, driver):
         driver.get("http://localhost/addressbook/")
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
+    def open_add_contact_page(self, driver):
+        driver.find_element_by_link_text("add new").click()
+
+    def create_contact(self, driver, contact):
+        driver.find_element_by_name("firstname").send_keys(contact.firstname)
+        driver.find_element_by_name("middlename").send_keys(contact.middlename)
+        driver.find_element_by_name("lastname").send_keys(contact.lastname)
+        driver.find_element_by_name("nickname").send_keys(contact.nickname)
+        driver.find_element_by_name("mobile").send_keys(contact.mobile)
+        driver.find_element_by_name("email").send_keys(contact.email)
+        driver.find_element_by_xpath("//div[@id='content']//input[1][@name='submit']").click()
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
