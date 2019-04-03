a38e812510bef26807baa6e5a08d3098fd4da8b7
stsarev <sergeytsarev90@gmail.com>
2019-04-03 16:24:10 +0300

    Задание 16 выполнено

--- fixture/application.py
+++ fixture/application.py
@@ -4,15 +4,25 @@ from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
-    def __init__(self):
-        self.driver = webdriver.Chrome()
+    def __init__(self,browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" %browser)
+
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
+
 
     def open_home_page(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
+        driver.get(self.base_url)
 
     def is_valid(self):
         try:
